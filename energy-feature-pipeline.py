60fccb2cd6af9bdde843ad00d7f94cf78783398f
Anton Bothin <abothin@kth.se>
2022-12-19 14:49:49 +0100

    more features from entso-e

--- energy-feature-pipeline.py
+++ energy-feature-pipeline.py
@@ -9,21 +9,28 @@ start_date = pd.Timestamp('20201219', tz='Europe/Berlin')
 end_date = pd.Timestamp('20221219', tz='Europe/Berlin')
 country_code = 'SE_3'
 
-batch_end_date = start_date + pd.Timedelta(days=300)
+batch_end_date = start_date + pd.Timedelta(days=100)
 
-i = 0
+energy_data = pd.DataFrame()
 
 while start_date < end_date:
-	result = client.query_day_ahead_prices(country_code, start=start_date, end=batch_end_date)
+	print(f'{start_date} - {batch_end_date}')
+
+	day_ahead_prices = client.query_day_ahead_prices(country_code, start=start_date, end=batch_end_date)
+	load = client.query_load(country_code, start=start_date, end=batch_end_date)
+	aggregate_water_reservoirs_and_hydro_storage = client.query_aggregate_water_reservoirs_and_hydro_storage(
+		country_code, start=start_date, end=batch_end_date)
+
+	result = pd.concat([day_ahead_prices, load, aggregate_water_reservoirs_and_hydro_storage], axis=1)
+	result = result.ffill()
+
+	energy_data = pd.concat([energy_data, result], axis=0)
 
 	start_date = batch_end_date + pd.Timedelta(days=1)
-	batch_end_date = start_date + pd.Timedelta(days=300)
+	batch_end_date = start_date + pd.Timedelta(days=100)
 	if batch_end_date > end_date:
 		batch_end_date = end_date
 
 	time.sleep(61)
 
-	result.to_csv(f'energy_{i}.csv')
-	i += 1
-
-
+energy_data.to_csv(f'energy.csv')
